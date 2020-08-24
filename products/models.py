d6b3fe63dbe98fc1de6675e90b0b72611a37a22c
vishkid84 <vishalittr@gmail.com>
2020-08-24 21:40:59 +0000

    customized admin

--- products/models.py
+++ products/models.py
@@ -2,6 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+        
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
@@ -25,4 +29,3 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
-        
