d47d38565c756c6da34133772b5656936b99d1c0
vishkid84 <vishalittr@gmail.com>
2020-09-16 19:35:49 +0000

    Updated allauth templates and tested profiles

--- profiles/models.py
+++ profiles/models.py
@@ -21,7 +21,7 @@ class UserProfile(models.Model):
 
 
 @receiver(post_save, sender=User)
-def create_or_update_user_profiles(sender, intance, created, **kwargs):
+def create_or_update_user_profile(sender, instance, created, **kwargs):
     """
     Create or update the user profile
     """
